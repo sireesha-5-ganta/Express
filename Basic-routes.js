c9044f66f1cc2a7d45b4dfc356238fd192486b5b
sireesha-5-ganta <sireeshaganta10@gmail.com>
2026-08-15 14:48:25 +0530

    Basic_routes

--- Basic-routes.js
+++ Basic-routes.js
@@ -1,18 +1,18 @@
 const express=require('express');
 const app=express();
 const PORT=5000;
-app.get("/", (req, res) => { res.send("Welcome to Student Application"); }); 
+app.get("/", (req, res) => { res.send("<h1>Welcome to Student Application</h1>"); }); 
  
 app.get("/student", (req, res) => { 
-    res.send("Student Name: Rahul, Roll No: 101, Department: CSE"); 
+    res.send("<h1>Student Name: Rahul, Roll No: 101, Department: CSE</h1>"); 
 }); 
 
 app.get("/courses", (req, res) => { 
-     res.send("Courses: JavaScript, Node.js, Database Management"); 
+     res.send("<h1>Courses: JavaScript, Node.js, Database Management</h1>"); 
 }); 
 
 app.get("/contact", (req, res) => {
-    res.send("<h1>Contact: student@example.com<h1>"); 
+    res.send("<h1>Contact: student@example.com</h1>"); 
 }); 
 
 app.listen(PORT, () => { 
